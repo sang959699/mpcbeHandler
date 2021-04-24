e17935ebf7cd1abbe2dbf1819398dd5d061359a7
YS Tan <sang959699@hotmail.com>
2021-04-24 19:13:25 +0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace mpcbeHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var param = args.Length == 0 ? String.Empty : args[0].Replace("mpcbe://", "");
+            var process = $"\"C:\\Program Files\\MPC-BE x64\\mpc-be64.exe\" \"{param}\"";
+            Process.Start(process);
+        }
+    }
+}
