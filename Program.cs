785cf3bc2c93fba08e08cfdbfadeb12c1bea1e67
YS <sang959699@hotmail.com>
2021-07-10 21:01:28 +0800

    Enhanced to GetSubtitle

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
 
 namespace mpcbeHandler
 {
@@ -7,9 +10,30 @@ namespace mpcbeHandler
     {
         static void Main(string[] args)
         {
-            var param = args.Length == 0 ? String.Empty : args[0].Replace("mpcbe://", "");
-            var process = $"\"C:\\Program Files\\MPC-BE x64\\mpc-be64.exe\" \"{param}\"";
+            var videoPath = args.Length == 0 ? String.Empty : args[0].Replace("mpcbe://", "");
+            var process = $"\"C:\\Program Files\\MPC-BE x64\\mpc-be64.exe\" \"{videoPath}\"";
+
+            var subtitle = GetSubtitle(videoPath);
+            if (subtitle != null) process += $" /sub \"{subtitle}\"";
+
             Process.Start(process);
+        }
+
+        private static string GetSubtitle(string path) {
+            path = HttpUtility.UrlDecode(path);
+            var fileName = Path.GetFileNameWithoutExtension(path);
+            string downloadPath = Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Downloads");
+            var allFiles = Directory.GetFileSystemEntries(downloadPath).ToList();
+            downloadPath += "\\";
+            string result = null;
+            for (int i = 0; i < allFiles.Count(); i++) {
+                allFiles[i] = allFiles[i].Replace(downloadPath, String.Empty);
+                if (allFiles[i].StartsWith(fileName)) {
+                    result = Path.Combine(downloadPath, allFiles[i]);
+                    break;
+                }
+            }
+            return result;
         }
     }
 }
